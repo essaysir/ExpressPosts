14c4c64dc98b2483832a0874bf680302827680e9
jsson <sonjs6789@gmail.com>
2024-06-12 13:48:26 +0900

    feat :  add get data logic and json data
    - json 데이터를 읽어서 , SSR 을 통해 페이지를 가져오도록 만듬

--- src/app/(ExpressPosts)/posts/page.tsx
+++ src/app/(ExpressPosts)/posts/page.tsx
@@ -1,6 +1,15 @@
 import ExpressPostsPage from '@/features/ExpressPosts/ExpressPostsPage';
+import path from 'path';
+import fs from 'fs';
 
-export default function Page() {
-  console.log('qwewq');
+async function getExpressPosts() {
+  const filePath = path.join(process.cwd(), 'public', 'data', 'ExpressData.json');
+  const jsonData = fs.readFileSync(filePath, 'utf8');
+  return JSON.parse(jsonData);
+}
+
+export default async function Page() {
+  const expressPosts = await getExpressPosts();
+  console.log(expressPosts);
   return <ExpressPostsPage />;
 }
