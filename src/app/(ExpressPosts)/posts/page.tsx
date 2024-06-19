aee46e7a63afac8194739785d99402ae3a752d05
jsson <sonjs6789@gmail.com>
2024-06-19 14:09:28 +0900

    feat : next-pwa 도입 시도

--- src/app/(ExpressPosts)/posts/page.tsx
+++ src/app/(ExpressPosts)/posts/page.tsx
@@ -1,15 +1,15 @@
 import ExpressPostsPage from '@/features/ExpressPosts/ExpressPostsPage';
-import path from 'path';
-import fs from 'fs';
-import { ExpressData } from '@/type/expressTypes';
+// import path from 'path';
+// import fs from 'fs';
+// import { ExpressData } from '@/type/expressTypes';
 
-async function getExpressPosts() {
-  const filePath = path.join(process.cwd(), 'public', 'data', 'ExpressData.json');
-  const jsonData = fs.readFileSync(filePath, 'utf8');
-  return JSON.parse(jsonData);
-}
+// async function getExpressPosts() {
+//   const filePath = path.join(process.cwd(), 'public', 'data', 'ExpressData.json');
+//   const jsonData = fs.readFileSync(filePath, 'utf8');
+//   return JSON.parse(jsonData);
+// }
 
 export default async function Page() {
-  const expressPosts = (await getExpressPosts()) as ExpressData;
-  return <ExpressPostsPage expressPosts={expressPosts} />;
+  // const expressPosts = (await getExpressPosts()) as ExpressData;
+  return <ExpressPostsPage />;
 }
--- src/features/ExpressPosts/ExpressPostsPage.tsx
+++ src/features/ExpressPosts/ExpressPostsPage.tsx
@@ -1,10 +1,13 @@
-import { ExpressData } from '@/type/expressTypes';
-
-export default function ExpressPostsPage({
-  expressPosts,
-}: {
-  expressPosts: ExpressData;
-}) {
-  console.log(expressPosts);
-  return <main className="w-full h-[100vh]" />;
+export default function ExpressPostsPage() {
+  return (
+    <main className="w-full h-[100vh] py-[5rem] px-[1rem]">
+      <section>
+        <h2 className="text-2xl font-bold">마포구 터미널 현황</h2>
+        <h4 className="font-bold">( 2024-06-18(수요일) 14:00 )</h4>
+        <div>
+          <div />
+        </div>
+      </section>
+    </main>
+  );
 }
